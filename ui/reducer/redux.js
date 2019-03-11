d2536335ea368ab682dcbf736244c3a41a270276
villerdex@gmail.com <villerdex@gmail.com>
2019-03-12 02:49:29 +0800

    remove add customer success in reducer

--- ui/reducer/redux.js
+++ ui/reducer/redux.js
@@ -8,10 +8,8 @@ export function reducer(state = initialState, action) {
 
   switch (action.type) {
     case "FETCH_CUSTOMER_LIST_SUCCESS":
-      console.log(action)
+      console.log('saga', action)
       return { ...state, customers: action.customers.customers };
-    case "ADD_CUSTOMER_SUCCESS":
-      return { ...state, customers: {...state.customers} }   
     default:
       return state;
   }
